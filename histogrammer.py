added phi wrapping and eta padding to seed images, added functionality to produce seed images in relative and/or physical units, as well as various small bug fixes

histogrammer.py
@@ -13,7 +13,7 @@ plt.style.use(mplhep.style.CMS)
 def setup_bins(nBins):
     eta_bins = np.linspace(-3, 3, nBins)
     phi_bins = np.linspace(-np.pi, np.pi, nBins)
-    bin_width = abs(eta_bins[1] - eta_bins[0]) / 2
+    bin_width = abs(eta_bins[-1] - eta_bins[-2])
     return eta_bins, phi_bins, bin_width
 
 
@@ -86,14 +86,15 @@ def find_local_maxima_with_secondary_exclude_adjacent(pts, N, minPt, exclusionRe
     return maxima_coords, secondary_coords, sums
 
 
-def plot_histogram(pts, eta_edges, phi_edges, maxima_coords, q, qb, eta_bins, phi_bins, ev, nBins, N):
+def plot_histogram(pts, eta_edges, phi_edges, maxima_coords, eta_bins, phi_bins, ev, nBins, N, q=None, qb=None):
     plt.figure(figsize=(12, 12), facecolor="white")
     plt.imshow(pts.T, origin='lower', aspect='auto',
                extent=[eta_edges[0], eta_edges[-1], phi_edges[0], phi_edges[-1]],
                cmap='viridis')
 
-    plt.scatter(q.eta, q.phi, s=500, color="white", marker="x", label="H decay quarks")
-    plt.scatter(qb.eta, qb.phi, s=500, color="white", marker="x")
+    if (q!=None) & (qb!=None):
+        plt.scatter(q.eta, q.phi, s=500, color="white", marker="x", label="H decay quarks")
+        plt.scatter(qb.eta, qb.phi, s=500, color="white", marker="x")
     
     bin_width_eta = eta_edges[1] - eta_edges[0]
     bin_width_phi = phi_edges[1] - phi_edges[0]

seed_images.py
@@ -7,27 +7,56 @@ import mplhep
 from histogrammer import setup_bins, create_histogram, find_local_maxima_with_secondary_exclude_adjacent
 plt.style.use(mplhep.style.CMS)
 
-def getImages(coords, pts, maskSize):
+def getImages(coords, pts, maskSize, rel=True, bw=1):
     imgs = []
+    eta_lim, phi_lim = len(pts[0]), len(pts[1])    # 36 for nBins=36
     for seed in coords:
         eta, phi = seed
         img = []
-        for e in np.arange(eta-math.floor(maskSize/2), eta+math.ceil(maskSize/2), 1):
-            for p in np.arange(phi-math.floor(maskSize/2), phi+math.ceil(maskSize/2), 1):
-                pt = pts[e][p]
-                img.append((int(e), int(p), pt))
+        etas = np.arange(eta - math.floor(maskSize/2), eta + math.ceil(maskSize/2), 1)
+        phis = np.arange(phi - math.floor(maskSize/2), phi + math.ceil(maskSize/2), 1)
+        if rel: rel_e = 0
+        for e in etas:
+            if rel: rel_p = 0
+            for p in phis:
+                # Phi wrapping
+                p_orig = p
+                p = p - phi_lim if p >= phi_lim else p
+                p = p + phi_lim if p < 0 else p
+             
+                if e >= eta_lim: pt = 0
+                elif e < 0: pt = 0
+                else: pt = pts[e][p]
+                    
+                if not rel:
+                    img.append((int(e)*bw, int(p_orig)*bw, pt))
+#                     img.append((int(e), int(p_orig), pt))
+                if rel:
+                    img.append((int(rel_e)*bw, int(rel_p)*bw, pt))
+#                     img.append((int(rel_e), int(rel_p), pt))
+                    rel_p += 1
+            
+            if rel: rel_e += 1
+                
         imgs.append(img)
 
     return np.array(imgs)
 
 
-def plotImage(imgs, jetIdx):
-    e, p, pt = zip(*imgs[jetIdx])
-    
+def plotImage(img):
+    e, p, pt = zip(*img)
+    maskSize=int(np.sqrt(len(pt)))
     plt.figure(figsize=(maskSize, maskSize), facecolor="white")
+    
+    bw = p[-1] - p[-2]
     plt.imshow(np.array(pt).reshape((maskSize, maskSize)).T, origin='lower', aspect='auto',
-               extent=[e[0], e[-1]+1, p[0], p[-1]+1],
+               extent=[e[0], e[-1]+bw, p[0], p[-1]+bw],
                cmap='viridis')
+    
+    plt.xticks( np.round(np.linspace(e[0], e[-1]+bw, maskSize+1), 2) )
+    plt.yticks( np.round(np.linspace(p[0], p[-1]+bw, maskSize+1), 2) )
+    
+    plt.xlabel("$\eta$"); plt.ylabel("$\phi$")
     plt.show()
     
     